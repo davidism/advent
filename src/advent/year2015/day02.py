ec85fe84263caaed161ba05c3dd398fb026be910
David Lord <davidism@gmail.com>
2019-12-08 12:21:53 -0800

    2015 02

---
+++ src/advent/year2015/day02.py
@@ -0,0 +1,29 @@
+from attr import dataclass
+
+from advent.load import read_input
+
+
+@dataclass(slots=True, frozen=True)
+class Present:
+    l: int
+    w: int
+    h: int
+
+    @classmethod
+    def from_line(cls, line: str) -> "Present":
+        return cls(*(int(i) for i in line.split("x")))
+
+    @property
+    def paper(self):
+        areas = (self.l * self.w, self.w * self.h, self.h * self.l)
+        return 2 * sum(areas) + min(areas)
+
+    @property
+    def ribbon(self):
+        l, w = sorted((self.l, self.w, self.h))[:2]
+        return 2 * (l + w) + self.l * self.w * self.h
+
+
+data = [Present.from_line(l) for l in read_input()]
+print(sum(p.paper for p in data))
+print(sum(p.ribbon for p in data))
