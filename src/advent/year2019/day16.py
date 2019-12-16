9fec4b3731e49baff4ae73aad41c40a914185619
David Lord <davidism@gmail.com>
2019-12-16 08:45:33 -0800

    2019 16 optimize

--- src/advent/year2019/day16.py
+++ src/advent/year2019/day16.py
@@ -1,5 +1,4 @@
 from itertools import accumulate
-from itertools import chain
 from itertools import cycle
 from itertools import repeat
 from test.test_itertools import take
@@ -11,12 +10,13 @@ from advent.load import read_input
 
 
 def next_phase(value, offset=False):
-    size = len(value)
-    half = size // 2
-    out = take(half, (x % 10 for x in accumulate(reversed(value))))
+    back_half = (x % 10 for x in accumulate(reversed(value)))
 
     if offset:
-        return list(chain(value[:half], reversed(out)))
+        return list(reversed(list(back_half)))
+
+    half = len(value) // 2
+    out = take(half, back_half)
 
     for i in range(half):
         pattern = cycle(flatten((repeat(x, half - i) for x in [0, 1, 0, -1])))
@@ -26,10 +26,12 @@ def next_phase(value, offset=False):
     return list(reversed(out))
 
 
-def n_phases(value, n, offset=False):
+def n_phases(value, n, offset=0):
+    if offset:
+        value = value[offset:]
+
     for _ in range(n):
-        print(_)
-        value = next_phase(value, offset)
+        value = next_phase(value, offset > 0)
 
     return value
 
@@ -38,4 +40,4 @@ data = [int(x) for x in read_input()]
 print("".join(str(x) for x in n_phases(data, 100)[:8]))
 offset = int("".join(str(x) for x in data[:7]))
 data *= 10000
-print("".join(str(x) for x in n_phases(data, 100, True)[offset : offset + 8]))
+print("".join(str(x) for x in n_phases(data, 100, offset)[:8]))
